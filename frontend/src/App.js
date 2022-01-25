8b6e7f10405c5dd168ee747ff7032809a767f54d
jim <shane47r@gmail.com>
2022-01-24 19:09:26 -0800

    Added boilerplate and tailwindcss config

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+import React, { useEffect } from 'react'; 
+import './App.css'; 
+
+import { BrowserRouter, Router, Switch } 'react-router-dom'; 
+
---
+++ frontend/src/components/auth/Signin.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react"; 
+import { useDispatch, useSelector } fron "react-redux"; 
+import { Redirect } from "react-router-dom"; 
+
+//import { makeStyles } from "@material-ui/core/styles";
+// Use tailwindcss instead
+
+
